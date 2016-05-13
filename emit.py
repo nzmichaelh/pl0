522a284f0568a6aa6199810022d3b1c8a2e8e3c5
Michael Hope <mlhx@google.com>
2016-05-13 22:35:40 +0200

    emit: split the C emitter out and added a p-Machine emitter.

--- emit.py
+++ emit.py
@@ -12,24 +12,81 @@ class Emitter:
         self.idx += 10
         return self.idx
 
+    def emit_program(self, program):
+        self.note('Program {}'.format(program))
+        self.start_program(program)
+        self.dispatch(program.block.consts)
+        self.dispatch(program.block.vars)
+        self.dispatch(program.block.procedures)
+        self.emit_body(program.block.statement)
+
+    def emit_block(self, block):
+        self.dispatch_children(block)
+
+    def emit_procedures(self, node):
+        self.dispatch_children(node)
+
+    def emit_compound(self, node):
+        self.dispatch_children(node)
+
+    def emit_procedure(self, proc):
+        self.enter_procedure(proc)
+        self.dispatch_children(proc)
+        self.exit_procedure(proc)
+
+    def emit_expression(self, expression):
+        for term in expression.terms.children.values():
+            self.dispatch(term)
+        for operation in expression.operations.children.values():
+            self.dispatch_operation(operation)
+
+    def emit_term(self, term):
+        for factor in term.factors.children.values():
+            self.dispatch(factor)
+        for operation in term.operations.children.values():
+            self.dispatch_operation(operation)
+
+    def emit_condition(self, cond):
+        self.dispatch(cond.left)
+        self.dispatch(cond.right)
+        self.dispatch_operation(cond.code)
+
+    def dispatch_children(self, node):
+        for child in node.children.values():
+            if isinstance(child, parser.Node):
+                self.dispatch(child)
+
+    def dispatch(self, node):
+        if node is None:
+            self.note('Skipping None')
+            return
+        target = 'emit_{}'.format(node.typename())
+        self.note('Invoking {}({})'.format(target, node))
+        getattr(self, target)(node)
+
+    def dispatch_operation(self, op):
+        self.note('Performing {}'.format(op))
+        name = op.val
+        if name == '#':
+            name = '!='
+
+        self.emit2(name)
+
+
+class CEmitter(Emitter):
     def cmd(self, msg):
         print(msg)
 
     def note(self, msg):
         print('    // {}'.format(msg))
 
-    def emit_program(self, program):
-        self.note('Program {}'.format(program))
+    def start_program(self, program):
         self.cmd('#include "pl0.h"')
-        self.dispatch(program.block.consts)
-        self.dispatch(program.block.vars)
-        self.dispatch(program.block.procedures)
-        self.cmd('void run() {')
-        self.dispatch(program.block.statement)
-        self.cmd('}')
 
-    def emit_block(self, block):
-        self.dispatch_children(block)
+    def emit_body(self, statement):
+        self.cmd('void run() {')
+        self.dispatch(statement)
+        self.cmd('}')
 
     def emit_vars(self, vars):
         for var in vars:
@@ -39,19 +96,29 @@ class Emitter:
         for name, val in vars.items():
             self.cmd('const int_t {} = {};'.format(name.val, val.val))
 
-    def emit_procedures(self, node):
-        self.dispatch_children(node)
-
-    def emit_compound(self, node):
-        self.dispatch_children(node)
-
-    def emit_procedure(self, proc):
+    def enter_procedure(self, proc):
         self.cmd('void {}() {{'.format(proc.name))
-        self.dispatch_children(proc)
+
+    def exit_procedure(self, proc):
         self.cmd('}')
 
     def emit_call(self, node):
         self.cmd('{}();'.format(node.ident.val))
+
+    def emit_assign(self, assign):
+        self.dispatch(assign.expr)
+        self.cmd('{} = pop();'.format(assign.ident.val))
+
+    def emit_number(self, token):
+        self.cmd('push({});'.format(token.val))
+
+    def emit_ident(self, token):
+        self.cmd('push({});'.format(token.val))
+
+    def emit2(self, cmd):
+        self.cmd(
+            '{{ int_t right = pop(); int_t left = pop(); push(left {} right); }}'.format(
+                cmd))
 
     def emit_write(self, node):
         self.dispatch(node.expression)
@@ -77,78 +144,91 @@ class Emitter:
         self.dispatch(node.expression)
         self.cmd('push(pop() & 1);')
 
-    def emit_condition(self, cond):
-        self.dispatch(cond.left)
-        self.dispatch(cond.right)
-        self.dispatch_operation(cond.code)
+
+class PMachineEmitter(Emitter):
+    def cmd(self, msg):
+        print(msg)
+
+    def note(self, msg):
+        print('    // {}'.format(msg))
+
+    def start_program(self, program):
+        self.cmd('#include "pl0.h"')
+
+    def emit_body(self, statement):
+        self.cmd('void run() {')
+        self.dispatch(statement)
+        self.cmd('}')
+
+    def emit_vars(self, vars):
+        for var in vars:
+            self.cmd('int_t {};'.format(var.val))
+
+    def emit_consts(self, vars):
+        for name, val in vars.items():
+            self.cmd('const int_t {} = {};'.format(name.val, val.val))
+
+    def enter_procedure(self, proc):
+        self.cmd('void {}() {{'.format(proc.name))
+
+    def exit_procedure(self, proc):
+        self.cmd('}')
+
+    def emit_call(self, node):
+        self.cmd('cup({});'.format(node.ident.val))
 
     def emit_assign(self, assign):
         self.dispatch(assign.expr)
-        self.cmd('{} = pop();'.format(assign.ident.val))
-
-    def emit_expression(self, expression):
-        for term in expression.terms.children.values():
-            self.dispatch(term)
-        for operation in expression.operations.children.values():
-            self.dispatch_operation(operation)
-
-    def emit_term(self, term):
-        for factor in term.factors.children.values():
-            self.dispatch(factor)
-        for operation in term.operations.children.values():
-            self.dispatch_operation(operation)
+        self.cmd('stl(&{});'.format(assign.ident.val))
 
     def emit_number(self, token):
-        self.cmd('push({});'.format(token.val))
+        self.cmd('ldci({});'.format(token.val))
 
     def emit_ident(self, token):
-        self.cmd('push({});'.format(token.val))
-
-    def emit_mul(self):
-        self.emit2('*')
-
-    def emit_add(self):
-        self.emit2('+')
-
-    def emit_lteq(self):
-        self.emit2('<=')
-
-    def emit_lt(self):
-        self.emit2('<')
-
-    def emit_gt(self):
-        self.emit2('>')
+        self.cmd('ldl(&{});'.format(token.val))
 
     def emit2(self, cmd):
-        self.cmd(
-            '{{ int_t right = pop(); int_t left = pop(); push(left {} right); }}'.format(
-                cmd))
+        REMAP = {
+            '+': 'adi',
+            '-': 'sbi',
+            '*': 'mpi',
+            '/': 'dvi',
+            '<=': 'leqi',
+            '>=': 'geqi',
+            '>': 'gti',
+            '<': 'lti',
+            '!=': 'neqi',
+        }
+        self.cmd('{}();'.format(REMAP[cmd]))
 
-    def dispatch_children(self, node):
-        for child in node.children.values():
-            if isinstance(child, parser.Node):
-                self.dispatch(child)
+    def emit_write(self, node):
+        self.dispatch(node.expression)
+        self.cmd('csp(write);')
 
-    def dispatch(self, node):
-        if node is None:
-            self.note('Skipping None')
-            return
-        target = 'emit_{}'.format(node.typename())
-        self.note('Invoking {}({})'.format(target, node))
-        getattr(self, target)(node)
+    def emit_while(self, node):
+        idx = self.next_id()
+        self.cmd('LABEL(while{});'.format(idx))
+        self.dispatch(node.condition)
+        self.cmd('FJP(while{}end);'.format(idx))
+        self.dispatch(node.statement)
+        self.cmd('UJP(while{});'.format(idx))
+        self.cmd('LABEL(while{}end);'.format(idx))
 
-    def dispatch_operation(self, op):
-        self.note('Performing {}'.format(op))
-        name = op.val
-        if name == '#':
-            name = '!='
+    def emit_if(self, node):
+        idx = self.next_id()
+        self.dispatch(node.condition)
+        self.cmd('FJP(if{});'.format(idx))
+        self.dispatch(node.statement)
+        self.cmd('LABEL(if{});'.format(idx))
 
-        self.emit2(name)
+    def emit_odd(self, node):
+        self.dispatch(node.expression)
+        self.cmd('odd();')
 
 
 def emit(program):
     program.dump('top')
-    emitter = Emitter()
+    emitter = PMachineEmitter()
     emitter.dispatch(program)
 
 

a8808932772f17531ab1effa474defab6d211af2
Michael Hope <mlhx@google.com>
2016-05-31 22:34:58 +0200

    util: split Node out into a utility class.

--- pl0/util.py
+++ pl0/util.py
@@ -33,3 +33,48 @@ class ReprMixin:
 
 def typename(obj):
     return obj.__class__.__name__.lower()
+
+
+class Node:
+    def __init__(self):
+        self._children = []
+
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            pass
+        elif name in self._children:
+            pass
+        else:
+            self._children.append(name)
+
+        super().__setattr__(name, value)
+
+    def __iter__(self):
+        return iter(getattr(self, x) for x in self._children)
+
+    def items(self):
+        return [(x, getattr(self, x)) for x in self._children]
+
+    def append(self, *vals):
+        for value in vals:
+            setattr(self, 'i{}'.format(len(self._children)), value)
+
+    def dump(self, name='', remain=-1, indent=0):
+        self._dump(name, self, remain, indent, set())
+
+    def _dump(self, name, item, remain, indent, seen):
+        if remain == 0:
+            return
+
+        if isinstance(item, (list, tuple)):
+            print('// {}{}: ['.format('  ' * indent, name))
+            for idx, i in enumerate(item):
+                self._dump('[{}]'.format(idx), i, remain, indent + 1, seen)
+        elif item in seen:
+            return
+        else:
+            seen.add(item)
+            print('// {}{}: {}'.format('  ' * indent, name, item))
+            if item is not None and isinstance(item, Node):
+                for key, child in item.items():
+                    self._dump(key, child, remain - 1, indent + 1, seen)
